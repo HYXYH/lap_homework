f5ae8a889d6062267f6dcf17315557e8fd28f1db
Насибуллин Оскар Рамилевич <oscarn08@gmail.com>
2019-03-28 20:29:41 +0300

    templates + long/2 fix

--- BinSearch/main.cpp
+++ BinSearch/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <vector>
-#include "bin_search.h"
+#include "bin_search.hpp"
 
 using namespace std;
 
-
+#include <limits>
 int main() {
 	int toFind;
 	vector<int> data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	cout << "input number to find:" << std::endl;
 	cin >> toFind;
 
-	bool isFound = bin_search(data.begin(), data.end(), toFind);
+
+
+	bool isFound = bin_search<vector<int>::iterator>(data.begin(), data.end(), toFind);
 	if(isFound){
 		cout << "Yep, " << toFind << " is found";
 	}
