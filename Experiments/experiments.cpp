56c61ee3400c8377f293c6e268592c222b734446
Насибуллин Оскар Рамилевич <oscarn08@gmail.com>
2019-04-14 22:04:33 +0300

    more experiments

--- Experiments/experiments.cpp
+++ Experiments/experiments.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 #include <iostream>
+
 struct EmptyStruct{
 
 };
@@ -14,6 +15,11 @@ struct BaseS{
   int64_t x{0};
 };
 
+struct BaseInverseS{
+  int64_t x{0};
+  char a{0};
+  char b{0};
+};
 
 struct ChildS1: public BaseS{
 
@@ -28,12 +34,20 @@ struct ChildS3: public BaseS{
   char d{0};
 };
 
+
+struct ChildS4: public BaseS, BaseInverseS {
+
+};
+
+
 int main(){
 
   std::cout << "size of empty struct: " << sizeof(EmptyStruct) << "\n";
   std::cout << "size of int64_t: " << sizeof(int64_t) << "\n";
   std::cout << "size of BaseS struct: " << sizeof(BaseS) << "\n";
+  std::cout << "size of BaseInverseS struct: " << sizeof(BaseInverseS) << "\n";
   std::cout << "size of ChildS1 struct: " << sizeof(ChildS1) << "\n";
   std::cout << "size of ChildS2 struct: " << sizeof(ChildS2) << "\n";
   std::cout << "size of ChildS3 struct: " << sizeof(ChildS3) << "\n";
+  std::cout << "size of ChildS4 struct: " << sizeof(ChildS4) << "\n";
 }
